b184bf77f0e04aa084a70113fcbddd55d5f47f51
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-03-19 23:07:25 +0200

    added tests for CalculatePrefix()

--- implementation_test.go
+++ implementation_test.go
@@ -7,17 +7,84 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type Test struct {
+	name       string
+	expression string
+	wanted     int
+	err        error
+}
+
+var Tests = []Test{
+	{
+		name:       "Plus",
+		expression: "+ 5 4",
+		wanted:     9,
+		err:        nil,
+	},
+	{
+		name:       "Minus",
+		expression: "- 22 12",
+		wanted:     10,
+		err:        nil,
+	},
+	{
+		name:       "Multiply",
+		expression: "* 8 7",
+		wanted:     56,
+		err:        nil,
+	},
+	{
+		name:       "Divide",
+		expression: "/ 72 9",
+		wanted:     8,
+		err:        nil,
+	},
+	{
+		name:       "Pow",
+		expression: "^ 3 4",
+		wanted:     81,
+		err:        nil,
+	},
+	{
+		name:       "Complex",
+		expression: "^ 2 * 2 / + 3 15 6",
+		wanted:     64,
+		err:        nil,
+	},
+	{
+		name:       "With spaces",
+		expression: "  ^ 2* 2    / +    3 15  6  ",
+		wanted:     64,
+		err:        nil,
+	},
+	{
+		name:       "NaN",
+		expression: "+ b 1 2 3",
+		wanted:     -1,
+		err:        fmt.Errorf("Error. Problem with opertator"),
+	},
+	{
+		name:       "Divide by 0",
+		expression: "/ 72 0",
+		wanted:     -1,
+		err:        fmt.Errorf("Error. Divide by zero"),
+	},
+}
+
 func TestCalculatePrefix(t *testing.T) {
-	res, err := CalculatePrefix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+	for _, tc := range Tests {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := CalculatePrefix(tc.expression)
+			assert.Equal(t, tc.err, err)
+			assert.Equal(t, tc.wanted, res)
+		})
 	}
 }
 
 func ExampleCalculatePrefix() {
-	res, _ := CalculatePrefix("+ 2 2")
+	res, _ := CalculatePrefix("+ 5 * - 4 2 3")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 11
 }
